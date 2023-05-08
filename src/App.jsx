8568b26b08319794818a87f7b469f877a0add0c3
tanvir-islam <2020tanvir19711@gmail.com>
2023-05-08 11:34:03 +0600

    all

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+import CoffeeCard from './components/CoffeeCard';
 
 const App = () => {
+  const loadedCoffees = useLoaderData();
+  const [coffees, setCoffees] = useState(loadedCoffees);
   return (
-    <div>
-      <form action="">
-        
-        <h1 className='text-2xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, tempore placeat odio explicabo iure amet earum, ducimus, labore architecto voluptatibus deleniti reiciendis! Recusandae sint deleniti odit nemo fugit, sapiente doloremque?</h1>
-      </form>
+    <div className='m-20'>
+    <h1 className='text-6xl text-center my-20 text-purple-600'>Hot Hot Cold Coffee: {coffees.length}</h1>
+    <div className='grid md:grid-cols-2 gap-4'>
+      {
+        coffees.map(coffee => <CoffeeCard
+          key={coffee._id}
+          coffee={coffee}
+          coffees={coffees}
+          setCoffees={setCoffees}
+        ></CoffeeCard>)
+      }
     </div>
+    <div className='text-center my-7'>
+      <Link to='/coffee'> <button className="btn btn-outline btn-success">ADD COFFEE</button> </Link>
+    </div>
+  </div>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,22 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import AddCoffee from './components/AddCoffee.jsx';
+import EditCoffee from './components/EditCoffee.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
+    loader: () => fetch('http://localhost:5000/coffees')
+  },
+  {
+    path: "/coffee",
+    element: <AddCoffee></AddCoffee>
+  },
+  {
+    path: "/updateCoffee/:id",
+    element: <EditCoffee></EditCoffee>,
+    loader: ({params}) => fetch(`http://localhost:5000/coffees/${params.id}`)
   },
 ]);
 
